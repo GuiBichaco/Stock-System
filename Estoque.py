2a55f9211e85f6c5ea9f845e1c45125b57093947
Guilherme <guicomercial328@gmail.com>
2023-05-26 21:41:49 -0300

    Creating Basic User ID

--- Estoque.py
+++ Estoque.py
@@ -1,2 +1,7 @@
+print("Welcome to the inventory manager !!")
+print("Please, inform your user !!")
+user = input()
+print("Please, enter your password !!")
+password = int(input())
 
-
+print(f"{user} you are on your management page!!")
